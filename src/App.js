1eb9d952cb2bb5f14853e4ccf3ecd680d265fd2e
Loreta <badalyan.loreta@gmail.com>
2024-03-26 01:35:37 +0400

    deleting an item

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 import { useState } from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
 
   function handleAddItems(item) {
     setItems((prevItems) => [item, ...prevItems]);
+  }
+
+  function handleRemoveItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleRemoveItem} />
       <Stats />
     </div>
   );
